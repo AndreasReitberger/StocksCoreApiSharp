3906ebd350ab269549d05444ef513fda30172bc7
Andreas <andy.reitberger@gmail.com>
2022-10-06 09:56:50 +0200

    Added AsyncLazy for using in Xamarin.Forms / MAUI

---
+++ src/StocksCoreApiSharp/StocksCoreApiSharp/Utilities/AsyncLazy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AndreasReitberger.Stocks.Utilities
+{
+    // https://learn.microsoft.com/de-de/xamarin/xamarin-forms/data-cloud/data/databases
+    public class AsyncLazy<T>
+    {
+        readonly Lazy<Task<T>> instance;
+
+        public AsyncLazy(Func<T> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        public AsyncLazy(Func<Task<T>> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return instance.Value.GetAwaiter();
+        }
+    }
+}
---
+++ src/StocksCoreApiSharp/StocksCoreApiSharp/Utilities/DatabaseHandler.Lazy.cs
@@ -0,0 +1,21 @@
+﻿#if SQLite
+namespace AndreasReitberger.Stocks.Utilities
+{
+    public partial class DatabaseHandler
+    {
+        #region Instance
+        public static AsyncLazy<DatabaseHandler> InstanceLazy => new(async () =>
+        {
+            DatabaseHandler instance;
+            lock (Lock)
+            {
+                instance = new DatabaseHandler();
+            }
+            await instance.InitTablesAsync();
+            return instance;
+        });
+
+        #endregion
+    }
+}
+#endif
